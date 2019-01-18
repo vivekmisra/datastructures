e2b245b6fef1cc4db963d60599441a878bed54a3
vivek <vivekmisrastudy2012@gmail.com>
2019-01-18 09:35:59 -0800

    ll

--- linkedlists/src/main/java/org/vivek/myinterview/linkedlists/core/SLL2.java
+++ linkedlists/src/main/java/org/vivek/myinterview/linkedlists/core/SLL2.java
@@ -3,6 +3,7 @@ package org.vivek.myinterview.linkedlists.core;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.NoSuchElementException;
+//import java.util.LinkedList.Node;
 
 public class SLL2<E> {
 	private static class Node<E> {
@@ -48,17 +49,26 @@ public class SLL2<E> {
 	}
 
 	Node<E> node(int index) {
-		// assert isElementIndex(index);
 		Node<E> node = null;
-		if (index < (length >> 1)) {
+		Node<E> nodeAtIndex = null;
+		if (index < length-1) {
 			node = first;
-			for (int i = 0; i < index; i++) {
-				node = node.next;
+			for (int i = 0; i < index; i++){
+				if(node.next!=null){
+					node  = node.next;
+				
+				}else{
+					node =null;
+				}
+			 
 			}
 
+		} else if (index >= length-1) {
+			node = last;
 		}
 		return node;
 	}
+
 	// ############################################END:GET######################################################################
 	// ############################################START:ADD/REMOVE#############################################################
 
@@ -73,6 +83,15 @@ public class SLL2<E> {
 
 	public void addLast(E e) {
 		linkLast(e);
+	}
+
+	public void add(int index, E element) {
+		if (index == length) {
+			linkLast(element);
+		} else {
+			linkBefore(element, node(index));
+		}
+
 	}
 
 	public E removeFirst() {
@@ -109,6 +128,46 @@ public class SLL2<E> {
 		return false;
 	}
 
+	Node<E> previous(Node<E> node) {
+		final Node<E> f = first;
+		Node<E> prev = null;
+		if (first == null) {
+			prev = null;
+		}
+		if (first.next == node) {
+			prev = first;
+
+		} else {
+			Node<E> temp = first;
+			while (first != null) {
+				if (first.next == node) {
+					prev = first;
+					break;
+				}
+				first = first.next;
+			}
+			first = temp;
+		}
+
+		return prev;
+
+	}
+
+	void linkBefore(E e, Node<E> succ) {
+		// assert succ != null;
+		final Node<E> pred = previous(succ);
+		final Node<E> newNode = new Node<>(e, succ);
+
+		if (pred == null){
+			first = newNode;
+		}else{
+			pred.next = newNode;
+			newNode.next = succ;
+		}
+		length++;
+
+	}
+
 	private void linkFirst(E e) {
 		final Node<E> f = first;
 		final Node<E> newNode = new Node<>(e, f);
@@ -135,32 +194,37 @@ public class SLL2<E> {
 	}
 
 	void linkLast(E e) {
-		final Node<E> l = last;
-		// final Node<E> newNode = new Node<>(l, e, null);
+		final Node<E> node = last;
 		final Node<E> newNode = new Node<>(e, null);
-		last = newNode;
-		if (l == null)
+
+		if (node == null) {// there is no last
 			first = newNode;
-		else
-			l.next = newNode;
+			last = newNode;
+		} else {
+			node.next = newNode;
+			last = newNode;
+		}
 		length++;
 
 	}
 
-	private E unlinkLast(Node<E> l) {
-		// assert l == last && l != null;
-		final E element = l.item;
+	private E unlinkLast(Node<E> node) {
+		final E element = node.item;
 		Node<E> prev = null;
-		l.item = null;
-		Node<E> temp = first;
-		while (first.next != null && first.next.next != null) {
-			prev = first.next;
-			first = first.next;
+		if (first == null) {
+			return null;
+		} else {
+			// get prev
+			Node<E> temp = first;
+			prev = previous(node);
+			node.item = null;// delink item
+			last = prev;
 		}
-		last = prev;
-		first = temp;// restore head
-
-		last.next = null;
+		if (prev == null) {
+			first = null;
+		} else {
+			prev.next = null;// now prev is last
+		}
 		length--;
 
 		return element;
@@ -168,29 +232,25 @@ public class SLL2<E> {
 
 	E unlink(Node<E> node) {
 		final E element = node.item;
+		final Node<E> next = node.next;
+		Node<E> prev = null;
 		if (first == null) {
 			return null;
+		} else {
+			// get prev
+			prev = previous(node);
+			node.item = null;// delink item
 		}
 
-		if (node.equals(first)) {
-			first = first.next;
-			// reduce the length of the list
-			length -= 1;
-			return first.item;
+		if (next == null) {
+			prev.next = null;
+			last = prev;
+			node = null;
+		} else {
+			prev.next = next;
+			node = null;
 		}
-		Node<E> p = first, q = null;
-		while ((q = p.next) != null) {
-			if (node.equals(q)) {
-				p.next = q.next;
-				// reduce the length of the list
-				length -= 1;
-				return p.item;
-			}
-			p = q;
-		}
-		
-		
-
+		length--;
 		return element;
 	}
 
@@ -201,8 +261,8 @@ public class SLL2<E> {
 				+ ", getFirst()=" + getFirst() + "]";
 	}
 
-	public void printLinkedList(Node head) {
-
+	public void printLinkedList(Node<E> head) {
+		Node<E> temp = head;
 		while (head != null) {
 			if (head.next != null) {
 				System.out.print(head.item + "->");
@@ -211,6 +271,7 @@ public class SLL2<E> {
 			}
 			head = head.next;
 		}
+		head = temp;
 		System.out.println();
 	}
 
@@ -229,28 +290,54 @@ public class SLL2<E> {
 
 		System.out.println("remove first:");
 		ll.removeFirst();
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
 
 		System.out.println("add first:");
 		ll.addFirst(4);
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
 
 		System.out.println("remove last:");
 		ll.removeLast();
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
 
 		System.out.println("add last:");
 		ll.addLast(1);
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
-
+        
 		System.out.println("add:");
 		ll.add(8);
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
 
-		System.out.println("remove:");
-		ll.remove(3);
+		System.out.println("add 6 at index 3:");
+		ll.add(3, 6);
+		System.out.println("size="+ll.size());
 		ll.printLinkedList(ll.first);
-		System.out.println(ll.size());
+
+		System.out.println("remove 3:");
+		ll.remove(3);
+		System.out.println("size="+ll.size());
+		ll.printLinkedList(ll.first);
+		
+
+		System.out.println("node at index 2:");
+		Node<Integer> n = ll.node(2);
+		System.out.println(n.item);
+		System.out.println("size="+ll.size());
+
+		System.out.println("node at index 6:");
+		n = ll.node(6);
+		System.out.println(n.item);
+		System.out.println("size="+ll.size());
+
+		System.out.println("add node(9) at index 7:");
+		ll.add(7, new Integer(9));
+		ll.printLinkedList(ll.first);
+		System.out.println("size="+ll.size());
 	}
 
 }
