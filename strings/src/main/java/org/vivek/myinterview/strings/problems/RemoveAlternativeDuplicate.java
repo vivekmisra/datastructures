9973d4ae42af13b687b73a330a2db23914df216c
vivek <vivekmisrastudy2012@gmail.com>
2019-01-24 21:13:47 -0800

    strings

---
+++ strings/src/main/java/org/vivek/myinterview/strings/problems/RemoveAlternativeDuplicate.java
@@ -0,0 +1,27 @@
+package org.vivek.myinterview.strings.problems;
+
+public class RemoveAlternativeDuplicate {
+
+	public static String removeAlternateDuplicates(String str) {
+		StringBuffer sb = new StringBuffer();
+		int charachters[] = new int[256];
+		char c;
+		for (int i = 0; i < str.length(); i++) {
+		    c = str.charAt(i);
+		    if (c >= 65 && c <= 90) {
+			c = ('a' - 'A');
+		    }
+		    if (charachters[c] == 0) {
+			charachters[c]++;
+			sb.append(str.charAt(i));
+		    } else {
+			charachters[c]--;
+		    }
+		}
+		return sb.toString();
+	    }
+	    public static void main(String[] args) {
+		String str = "Today is the day";
+		System.out.println(removeAlternateDuplicates(str));
+	    }
+}
